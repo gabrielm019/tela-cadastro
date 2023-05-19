a223a9bd407dabf3609d59663e237f4947412637
Gabriel Augusto <110576161+gabrielm019@users.noreply.github.com>
2023-05-18 21:55:17 -0300

    Update cad.php

--- cad.php
+++ cad.php
@@ -45,7 +45,7 @@ include "conexao.php";
             ">
                         <div class="card-body p-5 shadow-5 text-center">
                             <h2 class="fw-bold mb-5">Criar conta agora</h2>
-                            <form>
+                            <form method="post" action="login.ph">
                                 <!-- 2 column grid layout with text inputs for the first and last names -->
                                 <div class="row">
                                     <div class="col-md-6 mb-4">
